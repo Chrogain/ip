e12c0174be6eb88bc8cc402ffa046f2d9a1c7149
Chrogain <aidantan128@gmail.com>
2025-08-22 02:11:56 +0800

    Level 1. Echo. Echos the users command and exits with bye

--- src/main/java/Mel.java
+++ src/main/java/Mel.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Mel {
     public static void main(String[] args) {
         String line = "_______________________________________________________\n";
         String greeting = line
                 + "Hello! I'm Mel\n"
                 + "What can I do for you?\n"
-                + line
-                + "Bye! Hope to see you again soon!\n"
                 + line;
+
+        String exit_message = "Bye! Hope to see you again soon!\n";
         System.out.println(greeting);
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                break;
+
+            } else {
+                System.out.println(line + input + "\n" + line);
+
+            }
+        }
+
+        System.out.println(line + exit_message + line);
     }
 }
