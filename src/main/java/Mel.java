0b22cdaef2e1578b2363b366adc2ba91a5bc06d4
Chrogain <aidantan128@gmail.com>
2025-08-21 17:39:55 +0800

    Level-0: Change Duke.java to Mel.java and add new message

---
+++ src/main/java/Mel.java
@@ -0,0 +1,11 @@
+public class Mel {
+    public static void main(String[] args) {
+        String greeting = "_______________________________________________________\n"
+                + "Hello! I'm Mel\n"
+                + "What can I do for you?\n"
+                + "_______________________________________________________\n"
+                + "Bye! Hope to see you again soon!\n"
+                + "_______________________________________________________\n";
+        System.out.println(greeting);
+    }
+}
